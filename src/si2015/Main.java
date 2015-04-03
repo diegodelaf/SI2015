63b08e4f86bac3bc80c5ad2c767ad27b2a4acdec
diegodelaf <diegoldelafuente@gmail.com>
2015-04-03 14:28:26 -0600

    Commit desde eclipse.

--- src/si2015/Main.java
+++ src/si2015/Main.java
@@ -4,7 +4,7 @@ import lejos.nxt.Button;
 
 public class Main{
   public static void main (String[] args) {
-    System.out.println("Hello World");
+    System.out.println("Hola Mundo");
     Button.waitForAnyPress();
   }
 }
